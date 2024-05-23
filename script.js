178ecc10fa0d9bcc6f0a8f3c2bfcfab35b223cf1
Zach Robinson <zprobinson@sio.midco.net>
2024-05-22 22:36:02 -0500

    Add a game-log!

--- script.js
+++ script.js
@@ -12,29 +12,98 @@
         const userScore = document.querySelector("#userScore");
         const pcScore = document.querySelector("#pcScore");
         const imgs = document.querySelectorAll("img");
+        const gameLog = document.querySelector("#game-log");
         let n = 1; //round number
 
+        // It might be interesting to store all of the results of the games
+        // and show them on the screen.
+        // An array of objects that look like:
+        // {
+        //    playerChoice: "rock" | "paper" | "scissors",
+        //    computerChoice: "rock" | "paper" | "scissors",
+        //    winner: "You!" | "The Computer" | "Cat 🙀"
+        // }
+        const gameResults = [];
+
+        // Take the results of the game and add it to our game log!
+        const logGameResults = (playerSelection, computerSelection) => {
+            const gameDecision = decideGame(playerSelection, computerSelection);
+
+            // This is called a "ternary expression" and is a really terse way to do an if/else block
+            // x === y - this is the conditional check
+            // ? - this is what to do if TRUE
+            // : - this is what to do if false
+            const winner =
+              gameDecision === GameDecision.PlayerWin
+                // if TRUE, return the string 'YOU!'
+                ? "You!"
+                // if FALSE, then run this other conditional expression
+                : gameDecision === GameDecision.ComputerWin
+                // if TRUE, return the string 'The Computer'
+                ? "The Computer"
+                // if FALSE, then return the string 'Cat'
+                : "Cat 🙀";
+
+            const gameResult = {
+                playerChoice: playerSelection,
+                computerChoice: computerSelection,
+                winner: winner
+            }
+
+            gameResults.push(gameResult);
+        }
     
         function getComputerChoice(){
             let randomNum = Math.floor(Math.random()*3);
             return choiceArray.at(randomNum);
         }
 
-        // a funciton that plays the round and keeps scores
-        function playRound(playerSelection, computerSelection){ 
+        // We can move the logic for determining how a game is scored, and
+        // represent the result of that logic as an enumeration.
+        // Object.freeze is a function that means the object can't be mutated/changed.
+        // This is useful because we don't want to accidentally change the values of the object.
+        const GameDecision = Object.freeze({
+          PlayerWin: { name: "player" },
+          ComputerWin: { name: "computer" },
+          Tie: { name: "tie" },
+        });
+
+        // We don't need "else if" because we are returning from the function early if we find a match.
+        // Also, because this function does all of the "business logic", we can reuse it in multiple places.
+        // Like using it to play the round and update the UI, but also to log the game results!
+        const decideGame = (playerSelection, computerSelection) => {
             if ((playerSelection === "rock" && computerSelection === "scissors") ||
                 (playerSelection === "paper" && computerSelection === "rock") ||
                 (playerSelection === "scissors" && computerSelection === "paper")) {
-                playerScore++; 
-                roundResult.textContent = "WIN";
-                console.log(`player score is ${playerScore}`);
-            } else if (playerSelection === computerSelection) {
-                roundResult.textContent = "TIE";
-            } else {
-                computerScore++; 
-                console.log(`computer score is ${computerScore}`);
-                roundResult.textContent = "LOSE";
+              return GameDecision.PlayerWin;
             }
+            
+            if (playerSelection === computerSelection) {
+                return GameDecision.Tie
+            }
+
+            return GameDecision.ComputerWin
+        }
+
+        // a function that plays the round and keeps scores
+        function playRound(playerSelection, computerSelection){ 
+            const gameDecision = decideGame(playerSelection, computerSelection);
+
+            if (gameDecision === GameDecision.PlayerWin) {
+              playerScore++;
+              roundResult.textContent = "WIN";
+              console.log(`player score is ${playerScore}`);
+
+            } else if (gameDecision === GameDecision.ComputerWin) {
+                computerScore++;
+                roundResult.textContent = "LOSE";
+                console.log(`computer score is ${computerScore}`);
+                
+            } else {
+                roundResult.textContent = "TIE";
+            }
+
+            logGameResults(playerSelection, computerSelection);
         }
 
         function handleEvent() {
@@ -63,6 +132,8 @@
                 roundNum.textContent = "You Lost!";
                 resetGame();
             }
+            
+            createGameLog();
         }
         //a function that starts the game and announce the winner
         function playGame() {
@@ -78,7 +149,21 @@
             });
         }
              
-
+        // We need to generate list items for our unordered list
+        // based on the results of each game.
+        // By clearing all of the <li> elements and re-creating them,
+        // we guarantee that the list displayed on the screen always matches
+        // our gameResults array.
+        const createGameLog = () => {
+            gameLog.innerHTML = "";
+            gameResults.forEach((result, index) => {
+                const li = document.createElement("li");
+                const message = `Round ${index + 1}: You chose ${result.playerChoice}, Computer chose ${result.computerChoice}. Winner: ${result.winner}!`;
+                li.textContent = message;
+                li.style.marginBottom = "3px";
+                gameLog.appendChild(li);
+            });
+        }
 
         // Function that announces the winner at the end of the game
         function resetGame() {
@@ -100,7 +185,6 @@
         
         btn.addEventListener("click", () => {
             options.forEach(option => {
-                
                 option.style.display = "flex";
             });
             
@@ -112,8 +196,11 @@
             userChoiceDisplay.textContent = "n/a";
             pcChoiceDisplay.textContent = "n/a";
             roundNum.textContent = "";
+
+            // clear the log and reset the unordered list
+            gameLog.innerHTML = "";
+            gameResults.length = 0;
             
             playGame();
-
         });
         
